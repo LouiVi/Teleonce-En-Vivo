c1f66321398f3495dc6253407be7d420716b7569
LouiVi <ramirezdelarosalouisvincent@gmail.com>
2025-04-22 03:34:30 +0000

    Edited with DroidScript

---
+++ Telemundo en Vivo.js
@@ -0,0 +1,16 @@
+address = "https://www.telemundopr.com/en-vivo";
+
+//Called when application is started.
+function OnStart()
+{
+	//Create a layout with objects vertically centered.
+	lay = app.CreateLayout( "Linear", "VCenter,FillXY" )
+
+	//Create a text label and add it to layout.
+	web = app.CreateWebView(1, 1,  "IgnoreErrors,IgnoreSSLErrors" )
+	web.LoadUrl( address );
+	lay.AddChild( web )
+	
+	//Add layout to app.	
+	app.AddLayout( lay )
+}
